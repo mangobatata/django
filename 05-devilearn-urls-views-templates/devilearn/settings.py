646309ea16bf82200bd24294496b9baee2b5e69a
batata <contactobrite@gmail.com>
2026-04-29 18:58:03 +0000

    creando templates globales

--- 05-devilearn-urls-views-templates/devilearn/settings.py
+++ 05-devilearn-urls-views-templates/devilearn/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app.courses',
+    'apps.courses',
     'apps.profiles',
     'apps.dashboard',
 ]
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'devilearn.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
