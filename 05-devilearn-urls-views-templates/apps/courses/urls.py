c4e1761892058ba27425e4ca8366b6ec8a542829
mangobatata <contactobrite@gmail.com>
2026-04-30 23:07:10 -0300

    Delete 05-devilearn-urls-views-templates directory

--- 05-devilearn-urls-views-templates/apps/courses/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.course_list, name="course_list"),  # /courses
-    path("detail/", views.course_detail, name="course_detail"),
-    path("lessons/", views.course_lessons, name="course_lessons")
-]
--- 05-devilearn-urls-views-templates/static/js/multimedia.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll(".accordion-header").forEach((header) => {
-  header.addEventListener("click", () => {
-    const item = header.parentElement;
-    item.classList.toggle("active");
-  });
-});
